3f7abd9245de074d4c37d2d195ea67fe731de982
a <a@a.co>
2025-06-25 08:54:34 +0000

    Completed assignment 1

--- assignment1/q6/process_list.c
+++ assignment1/q6/process_list.c
@@ -1,17 +1,25 @@
 #include<linux/init.h>
 #include<linux/kernel.h>
 #include<linux/module.h>
+#include<linux/sched.h>
+#include<linux/sched/signal.h>
 
 MODULE_AUTHOR("Vedant Saini");
-MODULE_DESCRIPTION("Process list");
+MODULE_DESCRIPTION("Iterate through process list and print PID, STATE, NAME of each running process");
 
 static int __init process_list_init(void){
-	printk(KERN_INFO "Hello world\n");
+	struct task_struct *task;
+	rcu_read_lock();
+	pr_info("PID\tSTATE\tNAME\n");
+	for_each_process(task){
+		pr_info("%d\t%ld\t%s\n", task->pid, task->state, task->comm);
+	}
+	rcu_read_unlock();
 	return 0;
 }
 
 static void __exit process_list_exit(void){
-	printk(KERN_INFO "Bye!\n");
+	printk(KERN_INFO "Kaam khatam paisa hazam!\n");
 }
 
 
